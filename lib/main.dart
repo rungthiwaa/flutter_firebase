560a0fe1632da9da42ae5031043646daa774db59
rungthiwaa <ryawici@gmail.com>
2022-09-01 21:19:33 +0700

    สร้างแท็บ

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        body: TabBarView(
+          children: [Container(), Container()],
+        ),
+        backgroundColor: Colors.green,
+        bottomNavigationBar: TabBar(
+          tabs: [
+            Tab(
+              text: "บันทึกคะแนนสอบ",
+            ),
+            Tab(
+              text: "รายชื่อนักเรียน",
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
